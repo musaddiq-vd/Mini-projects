a8144f53a662fbde2382d1a2f352ba561db61330
musaddiq-vd <139722242+musaddiq-vd@users.noreply.github.com>
2026-09-07 16:59:12 +0530

    (better) Products of Array Except Self 238, space o(n)
    Implement productExceptSelf method using prefix and suffix arrays.

---
+++ DSA/Arrays/list/productExceptSelf_238_better.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+
+        n = len(nums)
+
+        # Prefix array
+        prefix = [1] * n
+
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+
+        # Suffix array
+        suffix = [1] * n
+
+        for i in range(n - 2, -1, -1):
+            suffix[i] = suffix[i + 1] * nums[i + 1]
+
+        # Final answer
+        result = [1] * n
+
+        for i in range(n):
+            result[i] = prefix[i] * suffix[i]
+
+        return result
+
